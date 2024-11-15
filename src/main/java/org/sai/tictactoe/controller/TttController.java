e15418dad8d0b703b2422acc2d5ff065d5e5567c
kaushik.sst@gmail.com <kaushik.sst@gmail.com>
2024-11-15 00:36:42 +0000

    Modifed the context for api

--- src/main/java/org/sai/tictactoe/controller/TttController.java
+++ src/main/java/org/sai/tictactoe/controller/TttController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @CrossOrigin
-@RestController("/")
+@RestController("/tictactoe")
 public class TttController {
 
     Result result;
